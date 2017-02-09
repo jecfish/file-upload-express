1e709bae7c7f8d09fa3040fb45ef1f320ba37873
chybie <chy.bie@htmail.com>
2017-02-10 01:14:20 +0800

    change to promise

--- index.ts
+++ index.ts
@@ -21,10 +21,12 @@ const upload = multer({
 const db = new Loki('uploads/images.json', { persistenceMethod: 'fs' });
 
 
-const loadCollection = function (colName, callback) {
-    db.loadDatabase({}, () => {
-        const _collection = db.getCollection(colName) || db.addCollection(colName);
-        callback(_collection);
+const loadCollection = function (colName): Promise<LokiCollection<any>> {
+    return new Promise(resolve => {
+        db.loadDatabase({}, () => {
+            const _collection = db.getCollection(colName) || db.addCollection(colName);
+            resolve(_collection);
+        })
     });
 }
 
@@ -40,41 +42,51 @@ app.post('/profile', upload.single('avatar'), (req, res) => {
     // req.file is the `avatar` file
     // req.body will hold the text fields, if there were any
     console.log(req.file, req.body)
-    loadCollection(COLLECTION_NAME, col => {
-        const data = col.insert(req.file);
-        db.saveDatabase();
-        res.send({ id: data.$loki, fileName: data.filename })
-    })
+
+    loadCollection(COLLECTION_NAME)
+        .then(col => {
+            const data = col.insert(req.file);
+            db.saveDatabase();
+            res.send({ id: data.$loki, fileName: data.filename })
+        })
+    // loadCollection(COLLECTION_NAME, col => {
+    //     const data = col.insert(req.file);
+    //     db.saveDatabase();
+    //     res.send({ id: data.$loki, fileName: data.filename })
+    // })
 })
 
 app.post('/photos/upload', upload.array('photos', 12), (req, res) => {
     // req.files is array of `photos` files
     // req.body will contain the text fields, if there were any
     console.log(req.files, req.body)
-    loadCollection(COLLECTION_NAME, col => {
-        let data = [];
-        (req.files as any).forEach(x => {
-            data = data.concat(col.insert(x));
-            db.saveDatabase();
+    loadCollection(COLLECTION_NAME)
+        .then(col => {
+            let data = [];
+            (req.files as any).forEach(x => {
+                data = data.concat(col.insert(x));
+                db.saveDatabase();
+            })
+            res.send(data.map(x => ({ id: x.$loki, fileName: x.filename })))
         })
-        res.send(data.map(x => ({ id: x.$loki, fileName: x.filename })))
-    })
 
 })
 
 app.get('/images', (req, res) => {
-    loadCollection(COLLECTION_NAME, col => {
-        res.send(col.data);
-    })
+    loadCollection(COLLECTION_NAME)
+        .then(col => {
+            res.send(col.data);
+        })
 })
 
 app.get('/images/:id', (req, res) => {
-    loadCollection(COLLECTION_NAME, col => {
-        const result = col.get(req.params.id);
-        if (!result) res.send(404);
-        res.setHeader('Content-Type', result.mimetype);
-        fs.createReadStream(path.join(UPLOAD_PATH, result.filename)).pipe(res);
-    })
+    loadCollection(COLLECTION_NAME)
+        .then(col => {
+            const result = col.get(req.params.id);
+            if (!result) res.send(404);
+            res.setHeader('Content-Type', result.mimetype);
+            fs.createReadStream(path.join(UPLOAD_PATH, result.filename)).pipe(res);
+        })
 })
 
 app.listen(3000, function () {
