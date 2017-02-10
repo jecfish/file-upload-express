7294b28d098472e05d55bb6fff124faeff2f33f2
chybie <chy.bie@htmail.com>
2017-02-10 23:36:35 +0800

    try catch all

--- index.ts
+++ index.ts
@@ -21,37 +21,53 @@ const app = express();
 app.use(cors());
 
 app.post('/profile', upload.single('avatar'), async (req, res) => {
-    const col = await loadCollection(COLLECTION_NAME, db);
-    const data = col.insert(req.file);
+    try {
+        const col = await loadCollection(COLLECTION_NAME, db);
+        const data = col.insert(req.file);
 
-    db.saveDatabase();
-    res.send({ id: data.$loki, fileName: data.filename });
+        db.saveDatabase();
+        res.send({ id: data.$loki, fileName: data.filename });
+    } catch (err) {
+        res.sendStatus(400);
+    }
 })
 
 app.post('/photos/upload', upload.array('photos', 12), async (req, res) => {
-    const col = await loadCollection(COLLECTION_NAME, db)
-    let data = col.insert(req.files);
+    try {
+        const col = await loadCollection(COLLECTION_NAME, db)
+        let data = [].concat(col.insert(req.files));
 
-    db.saveDatabase();
-    res.send(data.map(x => ({ id: x.$loki, fileName: x.filename })));
+        db.saveDatabase();
+        res.send(data.map(x => ({ id: x.$loki, fileName: x.filename })));
+    } catch (err) {
+        res.sendStatus(400);
+    }
 })
 
 app.get('/images', async (req, res) => {
-    const col = await loadCollection(COLLECTION_NAME, db);
-
-    res.send(col.data);
+    try {
+        const col = await loadCollection(COLLECTION_NAME, db);
+        res.send(col.data);
+    } catch (err) {
+        res.sendStatus(400);
+    }
 })
 
 app.get('/images/:id', async (req, res) => {
-    const col = await loadCollection(COLLECTION_NAME, db);
-    const result = col.get(req.params.id);
+    try {
+        const col = await loadCollection(COLLECTION_NAME, db);
+        const result = col.get(req.params.id);
 
-    if (!result) {
-        res.sendStatus(404);
-        return;
-    };
-    res.setHeader('Content-Type', result.mimetype);
-    fs.createReadStream(path.join(UPLOAD_PATH, result.filename)).pipe(res);
+        if (!result) {
+            res.sendStatus(404);
+            return;
+        };
+
+        res.setHeader('Content-Type', result.mimetype);
+        fs.createReadStream(path.join(UPLOAD_PATH, result.filename)).pipe(res);
+    } catch (err) {
+        res.sendStatus(400);
+    }
 })
 
 app.listen(3000, function () {
